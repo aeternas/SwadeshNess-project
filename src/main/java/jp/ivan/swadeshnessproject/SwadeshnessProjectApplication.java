fa61105f793daa1f41912bcfd57bf3d46f85dd9f
Ivan Golikov <ivgolikov7@gmail.com>
2020-09-23 21:41:58 +0300

    Initial commit

---
+++ src/main/java/jp/ivan/swadeshnessproject/SwadeshnessProjectApplication.java
@@ -0,0 +1,13 @@
+package jp.ivan.swadeshnessproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwadeshnessProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwadeshnessProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ivan/swadeshnessproject/SwadeshnessProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ivan.swadeshnessproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwadeshnessProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
