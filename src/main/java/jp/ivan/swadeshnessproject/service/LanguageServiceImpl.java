4794d0e67d65d28f1b1cc1e4c4451763f85eea80
Ivan Golikov <ivgolikov7@gmail.com>
2020-09-25 19:41:23 +0300

    Fixed interface conformance

--- src/main/java/jp/ivan/swadeshnessproject/service/LanguageServiceImpl.java
+++ src/main/java/jp/ivan/swadeshnessproject/service/LanguageServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class LanguageServiceImpl {
+public class LanguageServiceImpl implements LanguageService {
     @Autowired
     LanguageRepository languageRepository;
 
