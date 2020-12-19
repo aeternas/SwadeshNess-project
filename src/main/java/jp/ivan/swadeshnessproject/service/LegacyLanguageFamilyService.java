b417c676619b838f81001894356d70823b5229ec
Ivan Golikov <ivgolikov7@gmail.com>
2020-12-19 22:35:22 +0300

    Now services implement interfaces

--- src/main/java/jp/ivan/swadeshnessproject/service/LegacyLanguageFamilyService.java
+++ src/main/java/jp/ivan/swadeshnessproject/service/LegacyLanguageFamilyService.java
@@ -1,4 +1,14 @@
 package jp.ivan.swadeshnessproject.service;
 
-public class LegacyLanguageFamilyService {
+import jp.ivan.swadeshnessproject.entity.LanguageFamily;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LegacyLanguageFamilyService implements LanguageFamilyService {
+    @Override
+    public List<LanguageFamily> findAll() {
+        return null;
+    }
 }
--- src/main/java/jp/ivan/swadeshnessproject/service/LegacyLanguageService.java
+++ src/main/java/jp/ivan/swadeshnessproject/service/LegacyLanguageService.java
@@ -1,4 +1,12 @@
 package jp.ivan.swadeshnessproject.service;
 
-public class LegacyLanguageService {
+import jp.ivan.swadeshnessproject.entity.Language;
+
+import java.util.List;
+
+public class LegacyLanguageService implements LanguageService {
+    @Override
+    public List<Language> findAll() {
+        return null;
+    }
 }
